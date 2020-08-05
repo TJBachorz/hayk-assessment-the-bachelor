c6094a393da573d43f1e709332a8434d14041044
TJBachorz <TJBachorz@gmail.com>
2020-08-04 21:52:06 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,40 +5,65 @@ require 'json'
 file = File.read('./spec/fixtures/contestants.json')
 data = JSON.parse(file)
 
+def get_contestant_info_by_stat(data, stat, query, answer)
+  data.each do |season, info|
+    info.select do |contestant|
+      return contestant[answer] if contestant[stat] == query
+    end
+  end
+end
+
+def get_contestant_name(data, occupation)
+  get_contestant_info_by_stat(data, "occupation", occupation, 'name')
+end
 
 def get_first_name_of_season_winner(data, season)
-  winner = data[season].select do |contestant| 
-    contestant['status'] == "Winner"
+  winner = data[season].select do |contestant| contestant['status'] == "Winner"
   end
   winner[0]['name'].partition(" ").first
 end
 
+
+=begin
 def get_contestant_name(data, occupation)
   data.each do |season, info|
     info.select do |contestant| 
-      if contestant['occupation'] == occupation
-        return contestant['name']
-      end
+      return contestant['name'] if contestant['occupation'] == occupation
     end
   end
 end
+=end
+
 
 
 def count_contestants_by_hometown(data, hometown)
   residents = 0
   data.each do |season, info|
-    info.each do |contestants|
-      residents += 1 if contestants['hometown'].include? hometown
+    info.each do |contestant|
+      residents += 1 if contestant['hometown'].include? hometown
     end
   end
   residents
 end
 
-'''def get_occupation(data, hometown)
-  worker = data.select do |contestant| contestant['hometown']
 
-end'''
+=begin
+def get_occupation(data, hometown)
+  data.each do |season, info|
+    info.select do |contestant| 
+      return contestant['occupation'] if contestant['hometown'] == hometown
+    end
+  end
+end
+=end
+
+def get_occupation(data, hometown)
+  get_contestant_info_by_stat(data, "hometown", hometown, "occupation")
+end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  contestants = 0
+  data.each do |season, info|
+    info.select do |contestant|
 end
