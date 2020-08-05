a0467f7bd888c754eabbefff6b029eaf3cea4d75
TJBachorz <TJBachorz@gmail.com>
2020-08-05 16:36:20 -0600

    Complete all methods with more efficiency

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,75 +5,44 @@ require 'json'
 file = File.read('./spec/fixtures/contestants.json')
 data = JSON.parse(file)
 
-def get_contestant_info_by_stat(data, stat, query, answer)
+
+# INFORMATION RETRIEVAL METHODS ===========
+
+def all_contestants(data)
+  contestant_list = []
   data.each do |season, info|
-    info.select do |contestant|
-      return contestant[answer] if contestant[stat] == query
-    end
+    contestant_list.concat(info)
   end
+  contestant_list
+end
+ 
+def get_contestant_info_by_stat(data, stat, query, answer)
+  all_contestants(data).find {|contestant| contestant[stat] == query}[answer]
+end
+
+# ========================================
+
+def get_first_name_of_season_winner(data, season)
+  winner = data[season].select { |contestant| contestant['status'] == "Winner" }
+  winner[0]['name'].partition(" ").first
 end
 
 def get_contestant_name(data, occupation)
   get_contestant_info_by_stat(data, "occupation", occupation, 'name')
 end
 
-def get_first_name_of_season_winner(data, season)
-  winner = data[season].select do |contestant| contestant['status'] == "Winner"
-  end
-  winner[0]['name'].partition(" ").first
-end
-
-
-=begin
-def get_contestant_name(data, occupation)
-  data.each do |season, info|
-    info.select do |contestant| 
-      return contestant['name'] if contestant['occupation'] == occupation
-    end
-  end
-end
-
-
 def count_contestants_by_hometown(data, hometown)
-  residents = 0
-  data.each do |season, info|
-    info.each do |contestant|
-      residents += 1 if contestant['hometown'].include? hometown
-    end
+  contestants_from_location = all_contestants(data).select do |contestant|
+      contestant["hometown"].include? hometown
   end
-  residents
+  contestants_from_location.length
 end
-=end
-
-def count_contestants_by_hometown(data, hometown)
-  residents = 0
-  data.each do |season, info|
-    info.each do |contestant|
-      residents += 1 if contestant['hometown'].include? hometown
-    end
-  end
-  residents
-end
-
-
-=begin
-def get_occupation(data, hometown)
-  data.each do |season, info|
-    info.select do |contestant| 
-      return contestant['occupation'] if contestant['hometown'] == hometown
-    end
-  end
-end
-=end
 
 def get_occupation(data, hometown)
   get_contestant_info_by_stat(data, "hometown", hometown, "occupation")
 end
 
 def get_average_age_for_season(data, season)
-  ages = []
-  data[season].each do |info|
-    ages << info['age']
-  end
+  ages = data[season].map { |info| info['age'] }
   (ages.reduce(0) { |sum, n| sum + n.to_f } / ages.length).round()
 end
