393bd6dd82cecc3a6637608d56c754793ed3702d
TJBachorz <TJBachorz@gmail.com>
2020-08-04 18:52:35 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,16 +3,25 @@ require 'json'
 #require './spec/fixtures/contestants.json'
 
 file = File.read('./spec/fixtures/contestants.json')
-data = JSON.parse!(file)
+data = JSON.parse(file)
 
-puts data["season 19"]
 
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].select do |contestant|
+    contestant['status'] == "Winner"
+  end
+  winner[0]['name'].partition(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  person = []
+  data.each do |season, info|
+    info.select do |contestant| 
+      if contestant['occupation'] == occupation
+        return contestant['name']
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
